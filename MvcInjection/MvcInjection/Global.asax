c64e81a5c1df92b495b8f130369e2f16c8703c41
NuwanM <mnuwantharaka@gmail.com>
2018-10-07 14:28:12 +0530

    MVC application that uses Unity DI Framework

---
+++ MvcInjection/MvcInjection/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcInjection.MvcApplication" Language="C#" %>
---
+++ MvcInjection/MvcInjection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcInjection.Startup))]
+namespace MvcInjection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
